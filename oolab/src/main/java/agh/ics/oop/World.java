ab2e06b21d0d7608fa0ecac40d6046c854cc26d1
Filip <fpfpfp2002@gmail.com>
2022-10-12 10:47:29 +0200

    Zmiana sposobu radzenia sobie z błędnymi argumentami

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,5 +1,7 @@
 package agh.ics.oop;
 
+import java.util.Arrays;
+
 public class World {
     private static void run(Direction[] args){
         System.out.println();
@@ -18,17 +20,40 @@ public class World {
     }
 
     private static Direction[] switch_to_enum(String[] args){
+        int len = 0;
         Direction[] args_enum = new Direction[args.length];
         for(int i=0; i<args.length; i+=1){
-            args_enum[i] = switch(args[i]){
-                case "f" -> Direction.FORWARD;
-                case "b" -> Direction.BACKWARD;
-                case "l" -> Direction.LEFT;
-                case "r" -> Direction.RIGHT;
-                default -> Direction.OTHER;
-            };
+            switch (args[i]){
+                case "f":
+                    args_enum[len] = Direction.FORWARD;
+                    len += 1;
+                    break;
+                case "b":
+                    args_enum[len] = Direction.BACKWARD;
+                    len += 1;
+                    break;
+                case "l":
+                    args_enum[len] = Direction.LEFT;
+                    len += 1;
+                    break;
+                case "r":
+                    args_enum[len] = Direction.RIGHT;
+                    len += 1;
+                    break;
+                default:
+                    break;
+            }
         }
-        return args_enum;
+        Direction[] args_enum_copy = Arrays.copyOfRange(args_enum, 0, len);
+        return args_enum_copy;
+    }
+
+    private static void yield_the_arguments_to_the_console(String[] args){
+        for(int i=0; i<args.length-1; i+=1){
+            System.out.print(args[i]);
+            System.out.print(", ");
+        }
+        System.out.print(args[args.length-1]);
     }
 
     public static void main(String[] args) {
@@ -38,11 +63,7 @@ public class World {
             System.out.println("Zwierzak nie ma co robic, koncze dzialanie programu");
             System.exit(0);
         }
-        for(int i=0; i<args.length-1; i+=1){
-            System.out.print(args[i]);
-            System.out.print(", ");
-        }
-        System.out.print(args[args.length-1]);
+        yield_the_arguments_to_the_console(args);
         Direction[] args_enum = switch_to_enum(args);
         run(args_enum);
         System.out.println("system zakończył działanie");
